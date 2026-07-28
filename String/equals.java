facbf1a3e6138814189aedf6d716d2aa85041220
Prachi Chaurasia <your-github-email@example.com>
2026-07-28 23:14:14 +0530

    Added equals.java

---
+++ String/equals.java
@@ -0,0 +1,12 @@
+public class equals {
+    public static void main(String[] args) {
+        String s1 = "OpenAI";
+        String s2 = "OpenAI";
+
+        if (s1.equals(s2)) {
+            System.out.println("Strings are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+    }
+}
