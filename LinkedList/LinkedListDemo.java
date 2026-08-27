fb4f33705fdcf42eef8ed7c90274b21f7a5de1e6
Prachi Chaurasia <your-github-email@example.com>
2026-08-27 11:50:43 +0530

    Added LinkedListDemo.java

---
+++ LinkedList/LinkedListDemo.java
@@ -0,0 +1,28 @@
+class Node{ 
+    int data; 
+    Node next;
+}
+
+public class LinkedListDemo{
+    public static void main(String[] args) {
+        Node first = new Node();
+        Node second = new Node();
+        Node third = new Node();
+
+        first.data = 10;
+        second.data = 20;
+        third.data = 30;
+
+        first.next = second;
+        second.next = third;
+        third.next = null;
+
+        Node head = first;
+        Node current = head;
+        while(current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
+ 
